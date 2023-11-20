44ce2181406059f13991c1d84133d51fd3399c6e
makoto abe <abe00makoto@gmail.com>
2023-11-20 09:00:54 +0900

    Update path filter, refactor index.ts, remove config

--- index.ts
+++ index.ts
@@ -1,36 +1,44 @@
-
-import Recorder from './recorder';
-import Transcriber from './transcriber';
+import Recorder from "./recorder";
+import Transcriber from "./transcriber";
+import os from "os";
+import path from "path";
+import fs from "fs";
 
 const recorder = new Recorder();
 const transcriber = new Transcriber();
 
-const audioFilename = 'recording.mp3';
+
+const audioFilename = path.join(os.tmpdir(), "recording.mp3");
+process.on("exit", () => {
+  console.log("Removing audio file..."+ audioFilename);
+  fs.unlinkSync(audioFilename);
+});
 
 // 録音を開始する
 recorder.startRecording(audioFilename);
 
-// ユーザーの入力を待って録音を停止する
-process.stdin.on('data', async (data) => {
-  if (data.toString().trim() === 'stop') {
-    console.log('Stopping recording...');
-    
-    recorder.stopRecording();
-
-    // 音声を転写する
-    try {
-      const transcript = await transcriber.transcribe(audioFilename);
-      console.log(transcript);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error(`filed to transcribe: ${error.message}`);
-      } else {
-        console.error(`filed to transcribe : ${error}`);
-      }
-    } finally {
-      process.exit();
+const summarize = async () => {
+  console.log("録音を停止中...");
+  recorder.stopRecording();
+  console.log("Summarizing audio...");
+  try {
+    const transcript = await transcriber.transcribe(audioFilename);
+    console.log(transcript);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`filed to transcribe: ${error.message}`);
+    } else {
+      console.error(`filed to transcribe : ${error}`);
     }
-    
+  } finally {
+    process.exit();
   }
+};
+// ctrl+cもしくはctrl+dで録音を停止する
+process.on("SIGINT", async () => {
+  await summarize();
 });
 
+process.on("SIGTERM", async () => {
+  await summarize();
+});
